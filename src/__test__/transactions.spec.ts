d122bacaab155fe17f45398fe305f90b40a78699
Iago Alexandre <iagoaap@outlook.com>
2023-06-07 20:45:06 -0300

    create list all transactions test

--- src/__test__/transactions.spec.ts
+++ src/__test__/transactions.spec.ts
@@ -20,4 +20,28 @@ describe('transactions', () => {
 
     expect(response.statusCode).toEqual(201)
   })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'transactions',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const response = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(response.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'transactions',
+        amount: 5000,
+      }),
+    ])
+  })
 })
